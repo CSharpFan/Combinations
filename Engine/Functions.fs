da2d6d140a564b5b90f69abe01f991bdf4ebd48b
Kristof Mattei <github@kristofmattei.be>
2015-10-07 22:05:11 +0200

    Initial commit

---
+++ Engine/Functions.fs
@@ -0,0 +1,14 @@
+﻿module Engine.Functions
+
+let Calculate columns = 
+    let rec CalculateCombinations columns = 
+        match columns with
+        | h::[] -> [ [ h ] ]
+        | h::t -> 
+            let x = CalculateCombinations t
+            List.fold (fun acc elem -> (h::elem)::acc) ([h]::x) x
+        | [] -> []
+
+    columns 
+    |> CalculateCombinations
+    |> List.map set 
---
+++ Tests/CombinationTests.cs
@@ -0,0 +1,37 @@
+﻿namespace Tests
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using Engine;
+    using Microsoft.FSharp.Collections;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class CombinationTests
+    {
+        [TestMethod]
+        public void AssertAllCombinationsArePresent()
+        {
+            var result = Functions.Calculate(ListModule.OfSeq(new[] { 'A', 'B', 'C' })).ToList();
+
+            var expectedCombinations = new List<FSharpSet<char>>
+                                       {
+                                           new FSharpSet<char>(new[] { 'A' }),
+                                           new FSharpSet<char>(new[] { 'B' }),
+                                           new FSharpSet<char>(new[] { 'C' }),
+                                           new FSharpSet<char>(new[] { 'A', 'B' }),
+                                           new FSharpSet<char>(new[] { 'A', 'C' }),
+                                           new FSharpSet<char>(new[] { 'B', 'C' }),
+                                           new FSharpSet<char>(new[] { 'A', 'B', 'C' }),
+                                       }
+                ;
+
+            Assert.AreEqual(result.Count, expectedCombinations.Count);
+
+            foreach (var expectedCombination in expectedCombinations)
+            {
+                Assert.IsTrue(result.Contains(expectedCombination), "{0} was not found in the results");
+            }
+        }
+    }
+}
